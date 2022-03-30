07891cf4657b6618083dbd62716949a6fe704017
DeltaZ3ro <grishinzakhar1818@gmail.com>
2022-03-30 08:47:00 +0300

    Initial commit

---
+++ src/warehouse/DatabaseHandler.java
@@ -0,0 +1,21 @@
+package warehouse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler {
+    Connection dbConnection;
+
+    public Connection getDbConnection()
+            throws SQLException, ClassNotFoundException {
+        String connectionString = "jdbc:mysql://" +
+                "localhost:3306/warehouse" +  //IP, port, DB name
+                "?useUnicode=true&serverTimezone=UTC&useSSL=true&verifyServerCertificate=false";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        dbConnection = DriverManager.getConnection(connectionString, "root", "12345");
+        return dbConnection;
+    }
+}
